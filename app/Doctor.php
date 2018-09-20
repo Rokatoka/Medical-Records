0dcc1fa6ced925a203e4d168b279ba5f070c6c8f
Rokatoka <rollan.sharipov@nu.edu.kz>
2018-09-20 22:05:08 +0600

    new relationships

--- app/Doctor.php
+++ app/Doctor.php
@@ -6,5 +6,9 @@ use Illuminate\Database\Eloquent\Model;
 
 class Doctor extends Model
 {
-    //
+    protected $table = 'doctor';
+
+    public function treatment(){
+        return $this->hasMany('App\Treatment');
+    }
 }
